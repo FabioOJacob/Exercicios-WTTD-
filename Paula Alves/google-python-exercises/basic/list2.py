cf39ca7532c806cc33e5c3b63409d1497a954cfc
Paula Alves <paulafernandaalves@icloud.com>
2019-09-23 08:59:49 -0300

    Update01 230919

--- Paula Alves/google-python-exercises/basic/list2.py
+++ Paula Alves/google-python-exercises/basic/list2.py
@@ -14,8 +14,11 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+    list = []
+    for num in nums:
+        if len(list) == 0 or num != list[-1]:
+            list.append(num)
+    return list
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
